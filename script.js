f69335e7742485256896ea9f47b5feb63dbb2c1b
Ravi Yadav <yadavr74839@gmail.com>
2025-02-28 02:39:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,56 @@
+let userData = [];
+
+/// register logic ------------------ 
+
+
+
+/// login logic ---------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let emailValidate="@gmail.com"
+// let email = document.getElementById("email-username");
+// let password = document.getElementById("password");
+// let loginBtn = document.getElementById("login-btn");
+
+// loginBtn.addEventListener("click", function (event) {
+
+//   if (email.value.trim() === " ") {
+//     alert("pls enter email or username");
+//   }
+//   if(!email.value.trim().includes(emailValidate)){
+//     alert("invalid email or username")
+//   }
+  
+//   if (password.value.trim().length < 8) {
+//     alert("pls enter valid passowrd");
+//   }
+
+//   event.preventDefault();
+// });
